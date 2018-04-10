81c1ab7c357d4e188eb28931f50356a41dd2d6c7
Nicholas Blott <blottn@tcd.ie>
2018-04-10 16:26:32 +0100

    added simple tests

---
+++ test/verify.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var verify = require('../verify.js');
+describe('Verify',function() {
+	it('should import',function() {
+		assert.ok(verify);
+	});
+
+	describe('Broken Entities',function() {
+		var broken = {
+			i : {'a':0,'b':0,'c':0},
+			o : {'a':0,'b':0,'c':0},
+			run : function() {
+				console.log('example');
+			}
+		};
+	
+		it('should fail without name',function() {
+			assert.ok(!broken.name, broken.name);
+		});
+	});
+});
