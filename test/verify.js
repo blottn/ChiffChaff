1bdc9e3796aa7b82a6748acf0695f05cf8c01400
Nicholas Blott <blottn@tcd.ie>
2018-04-12 14:58:23 +0100

    added type and input tests

--- test/verify.js
+++ test/verify.js
@@ -1,21 +1,46 @@
 var assert = require('assert');
 var verify = require('../verify.js');
 describe('Verify',function() {
-	it('should import',function() {
+	it('Should import',function() {
 		assert.ok(verify);
 	});
 
 	describe('Broken Entities',function() {
-		var broken = {
+		var broken_name = {
+			type : 'broken',
 			i : {'a':0,'b':0,'c':0},
 			o : {'a':0,'b':0,'c':0},
 			run : function() {
 				console.log('example');
 			}
 		};
-	
-		it('should fail without name',function() {
-			assert.ok(!verify(broken),broken.name);
+
+		var broken_type = {
+			name : 'broken',
+			i : {'a':0,'b':0,'c':0},
+			o : {'a':0,'b':0,'c':0},
+			run : function() {
+				console.log('example');
+			}
+		};
+
+		var broken_i = {
+			name : 'broken',
+			type : 'broken',
+			o : {'a':0,'b':0,'c':0},
+			run : function() {
+				console.log('example');
+			}
+		}
+
+		it('Should fail without name',function() {
+			assert.ok(!verify(broken_name),'Should fail without name');
+		});
+		it('Should fail without type',function() {
+			assert.ok(!verify(broken_type),'Should fail without type');
+		});
+		it('Should fail without i',function() {
+			assert.ok(!verify(broken_i),'Should fail without name');
 		});
 	});
 });
