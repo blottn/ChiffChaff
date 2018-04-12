31ee477f0b0df709f455bc3f2f0cc34182fab872
Nicholas Blott <blottn@tcd.ie>
2018-04-12 19:21:01 +0100

    clean up test organisation, fixed error messages

--- test/verify.js
+++ test/verify.js
@@ -6,51 +6,64 @@ describe('Verify',function() {
 	});
 
 	describe('Broken Entities',function() {
-		var broken_name = {
-			type : 'broken',
-			i : {'a':0,'b':0,'c':0},
-			o : {'a':0,'b':0,'c':0},
-			run : function() {
-				console.log('example');
-			}
-		};
-		var broken_type = {
-			name : 'broken',
-			i : {'a':0,'b':0,'c':0},
-			o : {'a':0,'b':0,'c':0},
-			run : function() {
-				console.log('example');
-			}
-		};
-		var broken_i = {
-			name : 'broken',
-			type : 'broken',
-			o : {'a':0,'b':0,'c':0},
-			run : function() {
-				console.log('example');
-			}
-		}
-		var broken_o = {
-			name : 'broken',
-			type : 'broken',
-			i : {'a':0,'b':0,'c':0},
-			run : function() {
-				console.log('example');
-			}
-		}
-		it('Should fail without name',function() {
-			assert.ok(!verify(broken_name),'Should fail without name');
-			broken_name['name'] = ['test'];
-			assert.ok(!verify(broken_name),'Should fail with incorrect type of name');
+		describe('Name',function() {
+			var broken_name = {
+				type : 'broken',
+				i : {'a':0,'b':0,'c':0},
+				o : {'a':0,'b':0,'c':0},
+				run : function() {
+					console.log('example');
+				}
+			};
+			it('Should fail without name',function() {
+				assert.ok(!verify(broken_name),'Passed without name');
+			});
+			it('Should fail when not a string',function() {
+				broken_name['name'] = ['test'];
+				assert.ok(!verify(broken_name),'Passed with name as array');
+			});
 		});
-		it('Should fail without type',function() {
-			assert.ok(!verify(broken_type),'Should fail without type');
+		describe('Type',function() {
+			
+			var broken_type = {
+				name : 'broken',
+				i : {'a':0,'b':0,'c':0},
+				o : {'a':0,'b':0,'c':0},
+				run : function() {
+					console.log('example');
+				}
+			};
+			it('Should fail without type',function() {
+				assert.ok(!verify(broken_type),'Passed without type');
+			});
 		});
-		it('Should fail without i',function() {
-			assert.ok(!verify(broken_i),'Should fail without i');
+		
+		describe('Input',function() {
+			var broken_i = {
+				name : 'broken',
+				type : 'broken',
+				o : {'a':0,'b':0,'c':0},
+				run : function() {
+					console.log('example');
+				}
+			}
+			it('Should fail without i',function() {
+				assert.ok(!verify(broken_i),'Passed without i');
+			});
 		});
-		it('Should fail without o',function() {
-			assert.ok(!verify(broken_o),'Should fail without o');
+		
+		describe('Output',function() {
+			var broken_o = {
+				name : 'broken',
+				type : 'broken',
+				i : {'a':0,'b':0,'c':0},
+				run : function() {
+					console.log('example');
+				}
+			}
+			it('Should fail without o',function() {
+				assert.ok(!verify(broken_o),'Passed without o');
+			});
 		});
 	});
 });
