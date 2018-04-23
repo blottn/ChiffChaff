8cc5529add85ceeee5e8459217370bd1a7da172a
Nicholas Blott <blottn@tcd.ie>
2018-04-23 18:00:08 +0100

    added & tests

--- test/emu.js
+++ test/emu.js
@@ -7,4 +7,24 @@ describe('Emulation',function() {
 		assert.ok(verify,'Failed to import verify');
 		assert.ok(emu,'Failed to import emu');
 	});
+	describe('Map steps',function() {
+		it('Should work for &',function() {
+			var ent = {
+				name : 'name',
+				type : 'test',
+				i : {'a':1,'b':0},
+				o : {'x':0},
+				op : {
+					type : 'map',
+					data : {'x':'a&b'}
+				}
+			}
+			emu.step(ent);
+			assert.ok(ent.o.x === 0, '0 & 1 should be 0');
+
+			ent.i.b = 1;
+			emu.step(ent);
+			assert.ok(ent.o.x === 1, '1 & 1 should be 1');
+		});
+	});
 });
