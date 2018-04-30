f82ff55d41306fb126e18a845c2ed3d29ea777b6
Nicholas Blott <blottn@tcd.ie>
2018-04-30 14:14:21 +0100

    generalised tests for ops

--- test/emu.js
+++ test/emu.js
@@ -8,23 +8,74 @@ describe('Emulation',function() {
 		assert.ok(emu,'Failed to import emu');
 	});
 	describe('Map steps',function() {
-		it('Should work for &',function() {
-			let ent = {
-				name : 'name',
-				type : 'test',
-				i : {'a':1,'b':0},
-				o : {'x':0},
-				op : {
-					type : 'map',
-					data : {'x':'a&b'}
-				}
+		let ent = {
+			name : 'name',
+			type : 'test',
+			i : {'a':1,'b':0},
+			o : {'x':0},
+			op : {
+				type : 'map',
+				data : {'x':'a&b'}
 			}
-			emu.step(ent);
-			assert.ok(ent.o.x === 0, '0 & 1 should be 0');
-
-			ent.i.b = 1;
-			emu.step(ent);
-			assert.ok(ent.o.x === 1, '1 & 1 should be 1');
-		});
+		};
+		let conf = {
+			op : '&',
+			desc : 'a&b',
+			confs : [
+				{
+					i : {
+						a : 0,
+						b : 0
+					},
+					o : {
+						x : 0
+					},
+				},
+				{
+					i : {
+						a : 0,
+						b : 1
+					},
+					o : {
+						x : 0
+					},
+				},
+				{
+					i : {
+						a : 1,
+						b : 0
+					},
+					o : {
+						x : 0
+					},
+				},
+				{
+					i : {
+						a : 1,
+						b : 1
+					},
+					o : {
+						x : 1
+					},
+				}
+			]
+		};
+		run_for_op(conf,ent);
 	});
 });
+
+function run_for_op(op,ent) {
+	it ('Should work for ' + op.op, function() {
+		ent.op.data.x = op.desc;
+		for (let i in op.confs) {
+			for (let j in op.confs[i].i) {
+				ent.i[j] = op.confs[i].i[j];
+			}
+			emu.step(ent);
+			//assert for all outputs
+			for (let j in op.confs[i].o) {
+				assert.ok(ent.o[j] === op.confs[i].o[j],'Failed ' + op.desc);
+			}
+		}
+	});
+};
