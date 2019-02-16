0604f5ff8d73ff31210c243ab695329078fde4f9
Nicholas Blott <blottn@tcd.ie>
2019-02-16 09:19:19 +0000

    basic downloading

--- ide/editor.js
+++ ide/editor.js
@@ -1,3 +1,11 @@
+function save_contents(data, f_name) {
+    const dataType = 'data:text/plain;charset=utf-8'
+    let blob = encodeURIComponent(data);
+    $('#download_anchor').attr('href', dataType + blob)
+        .attr('download', f_name)
+        .click();
+}
+
 function init_editor() {
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
@@ -10,4 +18,5 @@ $(function () {
     // onload
     init_editor();
     
+    save_contents('hello world :D','chaffchaff.vhdl');
 });
