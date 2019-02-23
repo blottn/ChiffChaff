0c3aaa39ee97f0a404d0f2ecd24c257e1d2a072d
Nicholas Blott <blottn@tcd.ie>
2019-02-23 21:01:17 +0100

    added basic entity capture

--- ide/parse.js
+++ ide/parse.js
@@ -20,12 +20,35 @@ function getEntities(txt) {
         entities : [] 
     };
     
-    let entity = new RegExp(primitives.ENTITIY_START);
-    
+    let entity_regex;
+
+
+    entity_regex = new RegExp('entity\\s([A-Za-z]+\\w*)\\sis\\sPort(.*)(?=end\\s\\1)','gsm');
+    with(primitives) {
+        // find all signatures
+
+        let input = txt;
+        let ent_desc;
+        while (ent_desc = entity_regex.exec(input)) {
+            console.log(ent_desc[0]);
+        }
+        
+   /*     while (input != '' && input.indexOf(ENTITY_START >= 0)) {
+            let start = input.indexOf(ENTITY_START) + ENTITY_START.length;
+            if (start < input.length) {
+                 
+            }
+            else {
+                // fail parsing expected entity name
+            }
+        }*/
+    }
+
 }
 
 function parse(txt) {
     let obj = {};
+
     //TODO change to be more functional
     let commentless = stripComments(txt);
     let partParsed = getEntities(commentless);
