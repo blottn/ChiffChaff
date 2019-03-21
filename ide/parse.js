77aaa12186d21ce254ae0ef0f718cab0e966df10
Nicholas Blott <blottn@tcd.ie>
2019-03-21 15:38:39 +0000

    started build function

--- ide/parse.js
+++ ide/parse.js
@@ -17,13 +17,49 @@ function stripComments(txt) {
         .join('\n');
 }
 
+function build(ast) {
+    let kinds = {};
+    for (item of ast) {
+        if (item.type === 'entity') {
+            // build entity
+            let entity_ast = item.contents;
+            let ent = {
+                i: {},
+                o: {},
+                architecture: {
+                    internals: {},
+                    signals: {},
+                    logic: {}
+                }
+            };
+            for (mapping of entity_ast.mappings) {
+                let dir = mapping.dir === 'in' ? 'i' : 'o';
+                if (mapping.type.type === 'logic') {
+                    ent[dir][mapping.name] = 0;
+                }
+                else {
+                    let initial = [];
+                    for (var i = 0; i <= mapping.type.high; i++) {
+                        initial.push(0);
+                    }
+                    ent[dir][mapping.name] = initial;
+                }
+            }
+            kinds[entity_ast.def] = ent;
+        }
+        else if (item.type === 'architecture') {
+            console.log(item.contents);
+        }
+    }
+}
+
 function parse(txt) {
     let obj = {};
 
     //TODO change to be more functional
     let commentless = stripComments(txt);
-    console.log(JSON.stringify(program.parse(commentless).ast));
-    
+    console.log(program.parse(commentless));
+    build(program.parse(commentless).ast);
 }
 
-readFromFile('./ra.vhdl', parse);
+readFromFile('./fa.vhdl', parse);
