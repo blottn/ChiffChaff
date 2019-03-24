7ca007ba08b0d67ca73b3f04af583a1a67976cbd
Nicholas Blott <blottn@tcd.ie>
2019-03-24 22:01:58 +0000

    fixed data transformation

--- ide/src/page.js
+++ ide/src/page.js
@@ -1,34 +1,7 @@
 const itemBase = '<li class="list-group-item"><span style="display: inline; float: left;">Name</span><div height="50px;" style="display: inline; float: right;"></div></li>';
 
 function transformData(list) {
-    let y = [list[0]];
-    let x = [0];
-
-    let lastState = list[0];
-    let xVal = 1;
-    
-    let points = [];
-
-    for (let i = 1 ; i < list.length ; i++) {
-        let item = list[i];
-        if (item == lastState) {
-            x.push(xVal);
-            y.push(item);
-            points.push({x: xVal, y: item});
-        }
-        else {
-            y.push(lastState);
-            x.push(xVal);
-            points.push({x: xVal, y: lastState});
-            y.push(item);
-            x.push(xVal);
-            points.push({x: xVal, y: item});
-        }
-        xVal++;
-        lastState = item;
-    }
-    points.push({x: xVal, y:lastState});
-    return points;
+    return list.map((item, index) => {return {x: index, y: item};});
 }
 
 class Timings {
@@ -45,11 +18,12 @@ class Timings {
                
                 const width = 350;
                 const height = 32;
-
+                const margin = 4;
+                const lightGreen = '#78ff01';
                 let data = transformData([0,1,1,1,0,0,1,0]);
 
-                let yScale = d3.scaleLinear().range([height,0]);
-                let xScale = d3.scaleLinear().range([0,width]);
+                let yScale = d3.scaleLinear().range([height - margin, margin]);
+                let xScale = d3.scaleLinear().range([margin,width - margin]);
                 
                 let xAxis = d3.axisBottom()
                     .scale(xScale)
@@ -74,7 +48,8 @@ class Timings {
                     })
                     .y((d) => {
                         return yScale(d.y);
-                    });
+                    })
+                    .curve(d3.curveStep);
 
                 let svg = d3.select(div[0])
                     .append('svg')
@@ -82,12 +57,16 @@ class Timings {
                     .attr('width', width)
                     .attr('height', height)
                     .append('g')
-                    .attr('fill', 'black');
                 
+                svg.append('rect')
+                    .attr('fill', 'black')
+                    .attr('width', '100%')
+                    .attr('height', '100%');
+
                 svg.append('path')
                     .attr('d', line(data))
-                    .attr('stroke', 'blue')
-                    .attr('stroke-width', 3);
+                    .attr('stroke', lightGreen)
+                    .attr('stroke-width', 1);
 
                 root.appendChild(node[0]);
             }
