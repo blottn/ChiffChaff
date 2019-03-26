a87db7a80b9bcffb9dc46117d26f49389af387ba
Nicholas Blott <blottn@tcd.ie>
2019-03-26 09:13:06 +0000

    fixed indentation

--- ide/src/data.js
+++ ide/src/data.js
@@ -1,41 +1,42 @@
 // TODO delete
 // File of test data
-const vhdl = `entity full_adder_vhdl_code is
- Port ( A : in STD_LOGIC;
- B : in STD_LOGIC;
- Cin : in STD_LOGIC;
- S : out STD_LOGIC;
- Cout : out STD_LOGIC);
+const vhdl = `entity full_adder_vhdl_code is Port (
+    A : in STD_LOGIC;
+    B : in STD_LOGIC;
+    Cin : in STD_LOGIC;
+    S : out STD_LOGIC;
+    Cout : out STD_LOGIC);
 end full_adder_vhdl_code;
 
 architecture gate_level of full_adder_vhdl_code is
 
 begin
 
- S <= A XOR B XOR Cin ;
- Cout <= (A AND B) OR (Cin AND A) OR (Cin AND B) ;
+S <= A XOR B XOR Cin ;
+Cout <= (A AND B) OR (Cin AND A) OR (Cin AND B) ;
 
 end gate_level;
 
-entity Ripple_Adder is
-Port ( A : in STD_LOGIC;
-B : in STD_LOGIC;
-C : in STD_LOGIC;
-D : in STD_LOGIC;
-Cin : in STD_LOGIC;
-Y : out STD_LOGIC;
-Z : out STD_LOGIC;
-Cout : out STD_LOGIC);
+entity Ripple_Adder is Port (
+    A : in STD_LOGIC;
+    B : in STD_LOGIC;
+    C : in STD_LOGIC;
+    D : in STD_LOGIC;
+    Cin : in STD_LOGIC;
+    Y : out STD_LOGIC;
+    Z : out STD_LOGIC;
+    Cout : out STD_LOGIC);
 end Ripple_Adder; -- test in line comment
 
 architecture Behavioral of Ripple_Adder is
 
 -- Full Adder VHDL Code Component Decalaration
-component full_adder_vhdl_code Port ( A : in STD_LOGIC;
-                                      B : in STD_LOGIC;
-                                      Cin : in STD_LOGIC;
-                                      S : out STD_LOGIC;
-                                      Cout : out STD_LOGIC);
+component full_adder_vhdl_code Port (
+    A : in STD_LOGIC;
+    B : in STD_LOGIC;
+    Cin : in STD_LOGIC;
+    S : out STD_LOGIC;
+    Cout : out STD_LOGIC);
 end component;
 
 -- Intermediate Carry declaration
