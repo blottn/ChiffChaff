803015a8bf381c261eff29bb38b9040d91b22393
Nicholas Blott <blottn@tcd.ie>
2019-03-27 15:36:09 +0000

    fixed style _swish_

--- ide/src/page.js
+++ ide/src/page.js
@@ -1,15 +1,25 @@
 const itemBase = `<li class="list-group-item">
     <div class="container" style="padding:0px;">
     <div class="row" style="height:10%">
-    <div class="col-sm-2">
-        <p id="name" style="display: inline; float: left;">Name</p>
+    <div class="col-sm-3">
+        <div class="row" style="height:auto">
+            <div class="col-sm-7">
+                <p id="name" style="display: inline; float: left;">A</p>
+                <p style="display: inline;">:</p>
+            </div>
+            <div class="col-5" style="">
+        	    <div style="background-color:black;border-radius: 4px;text-align: center;">
+            	    <p id="state" style="margin: auto;color:lightgreen;">0</p>
+        	    </div>
+            </div>
+        </div>
     </div>
     <div class="col-sm-2">
         <button type="button" class="btn btn-info" style="display:none;">
             Flip
         </button>
     </div>
-    <div class="col-sm-8">
+    <div class="col-sm-7">
         <div id="svg-root" height="50px;" style="display: block;">
         </div>
         <p>T &rarr;</p>
@@ -48,16 +58,18 @@ class Timings {
             for (let i = 0 ; i < list.length ; i++) {
                 let node = $(itemBase);
                 let name = node.find('#name');
+                let state = node.find('#state');
                 let div = node.find('#svg-root');
                 name.text(list[i].name);
-
+                state.text(list[i].state);
                 if (flippable) {
                     node.find('button')
                         .attr('style', 'float:left;display:inline;')
-                    node.click(() => {
-                        list[i].state = list[i].state ^ 1;
-                        this.sim.flipped(list[i]);
-                    });
+                        .click(() => {
+                            list[i].state = list[i].state ^ 1;
+                            this.sim.flipped(list[i]);
+                            state.text(list[i].state);
+                        });
                 }
                
                 // initial data;
@@ -119,7 +131,7 @@ class Timings {
                     .attr('stroke-width', 1.5);
 
                 this.data[list[i].name].svg = svg;
-                this.data[list[i].name].root = div;
+                this.data[list[i].name].root = node;
                 this.data[list[i].name].line = line;
                 root.appendChild(node[0]);
             }
@@ -138,6 +150,8 @@ class Timings {
 
         let renderData = transformData(this.data[name].vals);
         let pathNode = root.find('path');
+        let stateNode = root.find('#state');
+        stateNode.text(state);
 
         pathNode.attr('d', this.data[name].line(renderData));
     }
