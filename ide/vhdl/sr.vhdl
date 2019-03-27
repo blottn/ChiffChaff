40c0ac4ba00d5fe43728fe1dd9d2f98e15be2b2b
Nicholas Blott <blottn@tcd.ie>
2019-03-27 22:05:22 +0000

    added sr latch

---
+++ ide/vhdl/sr.vhdl
@@ -0,0 +1,19 @@
+entity sr is Port (
+    S : in STD_LOGIC;
+    R : in STD_LOGIC;
+    O : out STD_LOGIC
+);
+end sr;
+
+architecture s of sr is
+
+signal q, nq : STD_LOGIC; 
+signal ps, pr : STD_LOGIC;
+begin
+
+O <= O;
+q <= (NOT R) AND (NOT nq);
+nq <= (NOT S) AND (NOT q);
+
+end s;
+
